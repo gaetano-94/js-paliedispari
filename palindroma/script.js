da9f8f9f0042723cb571b0d766af6cf6e4764931
gaetano-94 <gaetano.panico1994@gmail.com>
2023-11-14 16:02:03 +0100

    feat: parole palindrome

--- palindroma/script.js
+++ palindroma/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+/*
+1. Palidroma
+Chiedere all’utente di inserire una parola
+
+1) creare una variabile con un prompt, dove l'utente inserira una parola
+
+2) creare una funzione per capire se la parola inserita dall'utente e palindroma o no, quindi creando una variabile per rovesciare la parola inserita, per capire se effetivamente e palindroma 
+
+Creare una funzione per  rovesciare le parole, e creare un condizione che mi dica se la parola inserita è palindroma. Quindi true o false
+
+*/
+
+//1) creare una variabile con un prompt, dove l'utente inserira una parola
+const userInput = prompt('inserisci una parola');
+
+//2) creare una funzione per capire se la parola inserita dall'utente e palindroma o no, quindi creando una variabile per rovesciare la parola inserita, per capire se effetivamente e palindroma
+
+function ePalindroma(parola) {
+  const lowercaseParola = parola.toLowerCase();
+
+  const rovesciaParola = lowercaseParola.split('').reverse().join('');
+
+  return lowercaseParola === rovesciaParola;
+}
+
+if (ePalindroma(userInput)) {
+  console.log(`${userInput} è una parola palindroma`);
+} else {
+  console.log(`${userInput} non e una parola palidroma`);
+}
